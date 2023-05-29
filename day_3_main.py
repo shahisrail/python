93da6deaf487d402ee67665fcfbe834cfefd36af
shah israil <122022743+shahisrail@users.noreply.github.com>
2023-05-29 22:00:44 +0600

    Add files via upload

---
+++ day_3_main.py
@@ -0,0 +1,82 @@
+# all about List 
+# myList = [10,20,30,50,60,80,80]
+# myList.append('Mahbub')
+# myList.remove("Mahbub")
+# print(myList)
+# print(myList)
+# print(myList[6])
+
+# print(type(myList))
+# print(myList[0])
+# print(myList[1])
+# # -ve index
+# # print(myList[8])
+
+
+# Tuple 
+
+# myTuple = (10,20,3)
+# MyList = list(myTuple)
+# print(type(MyList))
+
+# print([MyList[0]])
+
+
+# for i in myTuple:
+#     print(i) 
+# # print(type(myTuple))
+
+# for j in myTuple:
+#     print(j)
+
+# Mylist1 = [10,205,30]
+# MyTuple2 = tuple(Mylist1)
+# print(type(MyTuple2))
+
+
+# Set 
+# s  = {100,'Mahbub Hussain',10.25}
+# for i in s:
+#     print(i)
+# s.remove(100)
+# # print(type(s))
+# # print(s[0])
+
+# print(s)
+
+# dict
+
+# myDict = {'fname':'Mahbub','lname':'Hussain',101:101}
+
+# print(myDict['fname'])
+# print(myDict[101])
+# print(type(myDict))
+
+
+# range form 1:
+# r = range(11)
+# for i in r:
+#     print(i)
+# range form 2:
+
+# rr = range(10,20)
+
+# for i in rr:
+#     print(i)
+
+
+# range form 3
+
+# r = range(10,21,5)
+# for i in r:
+#     print(i)
+
+
+#None data type 
+
+def m1():
+    a = 10
+
+print(m1())
+
+
